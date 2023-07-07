d90a0156a64bb440a2f4e5b47e755a87cfed4840
JinJiDeJinMu <1922295863@qq.com>
2023-07-07 17:36:23 +0800

    feature: add flink on k8s operator

---
+++ mythical-k8s/mythical-k8s/src/main/java/com/jm/mythical/k8s/flink/enums/FlinkMode.java
@@ -0,0 +1,20 @@
+package com.jm.mythical.k8s.flink.enums;
+
+public enum FlinkMode {
+    /**
+     * native application.
+     */
+    NATIVE_APPLICATION,
+    /**
+     * native session.
+     */
+    NATIVE_SESSION,
+    /**
+     * operator application.
+     */
+    OPERATOR_APPLICATION,
+    /**
+     * operator session.
+     */
+    OPERATOR_SESSION
+}
--- mythical-k8s/mythical-k8s/src/test/java/com/jm/mythical/k8s/MythicalK8sApplicationTests.java
+++ mythical-k8s/mythical-k8s/src/test/java/com/jm/mythical/k8s/MythicalK8sApplicationTests.java
@@ -1,20 +1,28 @@
 package com.jm.mythical.k8s;
 
 import com.jm.mythical.k8s.config.K8sClientConfig;
+import com.jm.mythical.k8s.service.IK8sDeploymentService;
 import com.jm.mythical.k8s.service.IK8sPodService;
 import io.fabric8.kubernetes.api.model.*;
+import org.apache.flink.kubernetes.operator.api.FlinkDeployment;
+import org.apache.flink.kubernetes.operator.api.spec.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class MythicalK8sApplicationTests {
 
     @Autowired
     IK8sPodService ik8sPodService;
+
+    @Autowired
+    IK8sDeploymentService ik8sDeploymentService;
 
 
     @javax.annotation.Resource
@@ -29,8 +37,6 @@ class MythicalK8sApplicationTests {
     public void getLog() {
         String log = client.getClient().pods().inNamespace("flink-operator").withName("tomcat-test").getLog();
         System.out.println(log);
-//        PodList flink = ik8sPodService.list("default");
-//        System.out.println(flink);
     }
 
     @Test
@@ -48,7 +54,7 @@ class MythicalK8sApplicationTests {
         containers.add(container);
 
         podSpec.setContainers(containers);
-        //podSpec.setServiceAccount("default");
+        podSpec.setServiceAccount("flink-operator");
 
         PodStatus podStatus = new PodStatus();
 
@@ -62,4 +68,57 @@ class MythicalK8sApplicationTests {
         Pod pod = ik8sPodService.create("flink-operator", podBuilder);
     }
 
+
+    @Test
+    public void operatorTest() {
+        FlinkDeployment flinkDeployment = new FlinkDeployment();
+        flinkDeployment.setApiVersion("flink.apache.org/v1beta1");
+        flinkDeployment.setKind("FlinkDeployment");
+        ObjectMeta objectMeta = new ObjectMeta();
+        objectMeta.setNamespace("flink-operator");
+        objectMeta.setName("basic-example");
+        flinkDeployment.setMetadata(objectMeta);
+        FlinkDeploymentSpec flinkDeploymentSpec = new FlinkDeploymentSpec();
+        flinkDeploymentSpec.setFlinkVersion(FlinkVersion.v1_16);
+        flinkDeploymentSpec.setImage("flink:1.16");
+        Map<String, String> flinkConfiguration = new HashMap<>();
+        flinkConfiguration.put("taskmanager.numberOfTaskSlots", "1");
+        flinkDeploymentSpec.setFlinkConfiguration(flinkConfiguration);
+        flinkDeployment.setSpec(flinkDeploymentSpec);
+        flinkDeploymentSpec.setServiceAccount("flink-operator");
+        JobManagerSpec jobManagerSpec = new JobManagerSpec();
+        jobManagerSpec.setResource(new Resource(1.0, "1024m", "2G"));
+        flinkDeploymentSpec.setJobManager(jobManagerSpec);
+        TaskManagerSpec taskManagerSpec = new TaskManagerSpec();
+        taskManagerSpec.setResource(new Resource(1.0, "1024m", "2G"));
+        flinkDeploymentSpec.setTaskManager(taskManagerSpec);
+        flinkDeployment
+                .getSpec()
+                .setJob(
+                        JobSpec.builder()
+                                .jarURI(
+                                        "local:///opt/flink/examples/streaming/StateMachineExample.jar")
+                                .parallelism(1)
+                                .upgradeMode(UpgradeMode.STATELESS)
+                                .build());
+
+
+        FlinkDeployment deployment = client.getClient().resource(flinkDeployment).create();
+
+    }
+
+
+    @Test
+    public void deleteFlinkDeployment() {
+        FlinkDeployment flinkDeployment = new FlinkDeployment();
+        flinkDeployment.setApiVersion("flink.apache.org/v1beta1");
+        flinkDeployment.setKind("FlinkDeployment");
+        ObjectMeta objectMeta = new ObjectMeta();
+        objectMeta.setNamespace("flink-operator");
+        objectMeta.setName("basic-example");
+
+
+        List<StatusDetails> delete = client.getClient().resource(flinkDeployment).delete();
+
+    }
 }
