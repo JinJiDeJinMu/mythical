200647a00ca601c0f8596edcf93d802308669397
JinJiDeJinMu <1922295863@qq.com>
2023-07-28 16:45:02 +0800

    feature: spark and flink test example

--- mythical-k8s/mythical-k8s/src/test/java/com/jm/mythical/k8s/MythicalK8sApplicationTests.java
+++ mythical-k8s/mythical-k8s/src/test/java/com/jm/mythical/k8s/MythicalK8sApplicationTests.java
@@ -75,18 +75,25 @@ class MythicalK8sApplicationTests {
 
     @Test
     public void flinkOperatorTest() {
-        FlinkDeployment flinkDeployment = new FlinkDeployment();
-        flinkDeployment.setApiVersion("flink.apache.org/v1beta1");
-        flinkDeployment.setKind("FlinkDeployment");
-        ObjectMeta objectMeta = new ObjectMeta();
-        objectMeta.setNamespace("flink-operator");
-        objectMeta.setName("basic-example");
-        flinkDeployment.setMetadata(objectMeta);
+        FlinkDeployment flinkDeployment = getFlinkDeployment("basic-example", "flink-operator");
+
+        // FlinkDeploymentSpec
         FlinkDeploymentSpec flinkDeploymentSpec = new FlinkDeploymentSpec();
         flinkDeploymentSpec.setFlinkVersion(FlinkVersion.v1_16);
         flinkDeploymentSpec.setImage("flink:1.16");
         Map<String, String> flinkConfiguration = new HashMap<>();
         flinkConfiguration.put("taskmanager.numberOfTaskSlots", "1");
+        /**
+         * checkpoints
+         */
+        flinkConfiguration.put("state.checkpoints.dir", "file:///opt/flink/checkpoints");
+        /**
+         * HA
+         */
+        flinkConfiguration.put("high-availability.type", "kubernetes");
+        flinkConfiguration.put("high-availability", "org.apache.flink.kubernetes.highavailability.KubernetesHaServicesFactory");
+        flinkConfiguration.put("high-availability.storageDir", "file:///opt/flink/flink_recovery");
+
         flinkDeploymentSpec.setFlinkConfiguration(flinkConfiguration);
         flinkDeployment.setSpec(flinkDeploymentSpec);
         flinkDeploymentSpec.setServiceAccount("flink-operator");
@@ -96,8 +103,16 @@ class MythicalK8sApplicationTests {
         TaskManagerSpec taskManagerSpec = new TaskManagerSpec();
         taskManagerSpec.setResource(new Resource(1.0, "1024m", "2G"));
         flinkDeploymentSpec.setTaskManager(taskManagerSpec);
-        flinkDeployment
-                .getSpec()
+
+        /**
+         * flink pod template
+         */
+        Pod podTemplate = new Pod();
+        podTemplate.setSpec(getPodSpec());
+
+        flinkDeploymentSpec.setPodTemplate(podTemplate);
+
+        flinkDeploymentSpec
                 .setJob(
                         JobSpec.builder()
                                 .jarURI(
@@ -111,18 +126,87 @@ class MythicalK8sApplicationTests {
 
     }
 
+    public PodSpec getPodSpec() {
+        PodSpec podSpec = new PodSpec();
+        List<Container> containers = new ArrayList<>();
+        containers.add(getPodSpecContainer());
 
-    @Test
-    public void deleteFlinkDeployment() {
+        podSpec.setContainers(containers);
+
+        //volumes
+        List<Volume> volumes = new ArrayList<>();
+        //log-volume
+        Volume log_volume = new Volume();
+        log_volume.setName("log-volume");
+        log_volume.setPersistentVolumeClaim(new PersistentVolumeClaimVolumeSource("flink-log-pvc ", false));
+
+        //ck-volume
+        Volume ck_volume = new Volume();
+        ck_volume.setName("checkpoints-volume");
+        ck_volume.setPersistentVolumeClaim(new PersistentVolumeClaimVolumeSource("flink-ck-pvc", false));
+
+        //ha-volume
+        Volume ha_volume = new Volume();
+        ha_volume.setName("ha-volume");
+        ha_volume.setPersistentVolumeClaim(new PersistentVolumeClaimVolumeSource("flink-ha-pvc", false));
+
+        volumes.add(log_volume);
+        volumes.add(ck_volume);
+        volumes.add(ha_volume);
+
+        podSpec.setVolumes(volumes);
+
+        return podSpec;
+    }
+
+    public Container getPodSpecContainer() {
+        Container container = new Container();
+        //env
+        List<EnvVar> envs = new ArrayList<>();
+        EnvVar envVar = new EnvVar();
+        envVar.setAdditionalProperty("TZ", "Asia/Shanghai");
+        envs.add(envVar);
+
+        container.setEnv(envs);
+        // volumeMounts
+        List<VolumeMount> volumeMounts = new ArrayList<>();
+
+        VolumeMount log = new VolumeMount();
+        log.setName("log-volume");
+        log.setMountPath("/opt/flink/log");
+
+        VolumeMount ck = new VolumeMount();
+        ck.setName("checkpoints-volume");
+        ck.setMountPath("/opt/flink/checkpoints");
+
+        VolumeMount ha = new VolumeMount();
+        ha.setName("ha-volume");
+        ha.setMountPath("/opt/flink/ha");
+
+        volumeMounts.add(log);
+        volumeMounts.add(ha);
+        volumeMounts.add(ck);
+
+        container.setVolumeMounts(volumeMounts);
+
+        return container;
+    }
+
+    FlinkDeployment getFlinkDeployment(String name, String namespace) {
         FlinkDeployment flinkDeployment = new FlinkDeployment();
         flinkDeployment.setApiVersion("flink.apache.org/v1beta1");
         flinkDeployment.setKind("FlinkDeployment");
         ObjectMeta objectMeta = new ObjectMeta();
-        objectMeta.setNamespace("flink-operator");
-        objectMeta.setName("basic-example");
+        objectMeta.setNamespace(namespace);
+        objectMeta.setName(name);
 
+        return flinkDeployment;
+    }
 
-        List<StatusDetails> delete = client.getClient().resource(flinkDeployment).delete();
+    @Test
+    public void deleteFlinkDeployment() {
+
+        List<StatusDetails> delete = client.getClient().resource(getFlinkDeployment("basic-example", "flink-operator")).delete();
 
     }
 
@@ -157,11 +241,11 @@ class MythicalK8sApplicationTests {
                 SparkApplicationSpec.Builder()
                         .type("Scala")
                         .mode("cluster")
-                        .image("https://archive.apache.org/dist/spark/spark-3.1.1")
+                        .image("apache/spark:v3.3.1")
                         .imagePullPolicy("Always")
                         .mainClass("org.apache.spark.examples.SparkPi")
-                        .mainApplicationFile("local:///opt/spark/examples/jars/spark-examples_2.12-3.1.1.jar")
-                        .sparkVersion("3.1.1")
+                        .mainApplicationFile("local:///opt/spark/examples/jars/spark-examples_2.12-3.3.1.jar")
+                        .sparkVersion("3.3.1")
                         .restartPolicy(new RestartPolicy("Never"))
                         .driver(driver)
                         .executor(executor)
